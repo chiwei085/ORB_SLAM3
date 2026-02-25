7372bd4afb691dad19f4afe4f98f849429996dfa
chiwei085 <yehchiwei.work@gmail.com>
2026-02-26 06:37:40 +0800

    [atlas] add streaming iterators for map/atlas

--- include/Map.h
+++ include/Map.h
@@ -1,34 +1,35 @@
 /**
-* This file is part of ORB-SLAM3
-*
-* Copyright (C) 2017-2021 Carlos Campos, Richard Elvira, Juan J. Gómez Rodríguez, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
-* Copyright (C) 2014-2016 Raúl Mur-Artal, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
-*
-* ORB-SLAM3 is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
-* License as published by the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ORB-SLAM3 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
-* the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License along with ORB-SLAM3.
-* If not, see <http://www.gnu.org/licenses/>.
-*/
-
+ * This file is part of ORB-SLAM3
+ *
+ * Copyright (C) 2017-2021 Carlos Campos, Richard Elvira, Juan J. Gómez
+ * Rodríguez, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
+ * Copyright (C) 2014-2016 Raúl Mur-Artal, José M.M. Montiel and Juan D. Tardós,
+ * University of Zaragoza.
+ *
+ * ORB-SLAM3 is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * ORB-SLAM3 is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ORB-SLAM3. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef MAP_H
 #define MAP_H
 
-#include "MapPoint.h"
-#include "KeyFrame.h"
-
-#include <set>
-#include <pangolin/pangolin.h>
-#include <mutex>
-
 #include <boost/serialization/base_object.hpp>
+#include <pangolin/pangolin.h>
 
+#include <mutex>
+#include <set>
+
+#include "KeyFrame.h"
+#include "MapPoint.h"
 
 namespace ORB_SLAM3
 {
@@ -42,29 +43,29 @@ class Map
 {
     friend class boost::serialization::access;
 
-    template<class Archive>
-    void serialize(Archive &ar, const unsigned int version)
-    {
-        ar & mnId;
-        ar & mnInitKFid;
-        ar & mnMaxKFid;
-        ar & mnBigChangeIdx;
+    template <class Archive>
+    void serialize(Archive& ar, const unsigned int version) {
+        ar& mnId;
+        ar& mnInitKFid;
+        ar& mnMaxKFid;
+        ar& mnBigChangeIdx;
 
-        // Save/load a set structure, the set structure is broken in libboost 1.58 for ubuntu 16.04, a vector is serializated
-        //ar & mspKeyFrames;
-        //ar & mspMapPoints;
-        ar & mvpBackupKeyFrames;
-        ar & mvpBackupMapPoints;
+        // Save/load a set structure, the set structure is broken in
+        // libboost 1.58 for ubuntu 16.04, a vector is serializated
+        // ar & mspKeyFrames;
+        // ar & mspMapPoints;
+        ar& mvpBackupKeyFrames;
+        ar& mvpBackupMapPoints;
 
-        ar & mvBackupKeyFrameOriginsId;
+        ar& mvBackupKeyFrameOriginsId;
 
-        ar & mnBackupKFinitialID;
-        ar & mnBackupKFlowerID;
+        ar& mnBackupKFinitialID;
+        ar& mnBackupKFlowerID;
 
-        ar & mbImuInitialized;
-        ar & mbIsInertial;
-        ar & mbIMU_BA1;
-        ar & mbIMU_BA2;
+        ar& mbImuInitialized;
+        ar& mbIsInertial;
+        ar& mbIMU_BA1;
+        ar& mbIMU_BA2;
     }
 
 public:
@@ -77,7 +78,7 @@ public:
     void AddMapPoint(MapPoint* pMP);
     void EraseMapPoint(MapPoint* pMP);
     void EraseKeyFrame(KeyFrame* pKF);
-    void SetReferenceMapPoints(const std::vector<MapPoint*> &vpMPs);
+    void SetReferenceMapPoints(const std::vector<MapPoint*>& vpMPs);
     void InformNewBigChange();
     int GetLastBigChangeIdx();
 
@@ -86,7 +87,31 @@ public:
     std::vector<MapPoint*> GetReferenceMapPoints();
 
     long unsigned int MapPointsInMap();
-    long unsigned  KeyFramesInMap();
+    long unsigned KeyFramesInMap();
+    inline auto MapPointsInMapRaw() const -> size_t {
+        std::unique_lock<std::mutex> lock(const_cast<Map*>(this)->mMutexMap);
+        return mspMapPoints.size();
+    }
+    inline auto KeyFramesInMapRaw() const -> size_t {
+        std::unique_lock<std::mutex> lock(const_cast<Map*>(this)->mMutexMap);
+        return mspKeyFrames.size();
+    }
+
+    template <class Fn>
+    void ForEachMapPoint(Fn&& fn) const {
+        std::unique_lock<std::mutex> lock(const_cast<Map*>(this)->mMutexMap);
+        for (auto* mp : mspMapPoints) {
+            fn(mp);
+        }
+    }
+
+    template <class Fn>
+    void ForEachKeyFrame(Fn&& fn) const {
+        std::unique_lock<std::mutex> lock(const_cast<Map*>(this)->mMutexMap);
+        for (auto* kf : mspKeyFrames) {
+            fn(kf);
+        }
+    }
 
     long unsigned int GetId();
 
@@ -115,7 +140,8 @@ public:
     void SetImuInitialized();
     bool isImuInitialized();
 
-    void ApplyScaledRotation(const Sophus::SE3f &T, const float s, const bool bScaledVel=false);
+    void ApplyScaledRotation(const Sophus::SE3f& T, const float s,
+                             const bool bScaledVel = false);
 
     void SetInertialSensor();
     bool IsInertial();
@@ -130,17 +156,24 @@ public:
 
     unsigned int GetLowerKFID();
 
-    void PreSave(std::set<GeometricCamera*> &spCams);
-    void PostLoad(KeyFrameDatabase* pKFDB, ORBVocabulary* pORBVoc/*, map<long unsigned int, KeyFrame*>& mpKeyFrameId*/, map<unsigned int, GeometricCamera*> &mpCams);
+    void PreSave(std::set<GeometricCamera*>& spCams);
+    void PostLoad(
+        KeyFrameDatabase* pKFDB,
+        ORBVocabulary*
+            pORBVoc /*, map<long unsigned int, KeyFrame*>& mpKeyFrameId*/,
+        map<unsigned int, GeometricCamera*>& mpCams);
 
-    void printReprojectionError(list<KeyFrame*> &lpLocalWindowKFs, KeyFrame* mpCurrentKF, string &name, string &name_folder);
+    void printReprojectionError(list<KeyFrame*>& lpLocalWindowKFs,
+                                KeyFrame* mpCurrentKF, string& name,
+                                string& name_folder);
 
     vector<KeyFrame*> mvpKeyFrameOrigins;
     vector<unsigned long int> mvBackupKeyFrameOriginsId;
     KeyFrame* mpFirstRegionKF;
     std::mutex mMutexMapUpdate;
 
-    // This avoid that two points are created simultaneously in separate threads (id conflict)
+    // This avoid that two points are created simultaneously in separate threads
+    // (id conflict)
     std::mutex mMutexPointCreation;
 
     bool mbFail;
@@ -156,13 +189,13 @@ public:
     std::set<long unsigned int> msFixedKFs;
 
 protected:
-
     long unsigned int mnId;
 
     std::set<MapPoint*> mspMapPoints;
     std::set<KeyFrame*> mspKeyFrames;
 
-    // Save/load, the set structure is broken in libboost 1.58 for ubuntu 16.04, a vector is serializated
+    // Save/load, the set structure is broken in libboost 1.58 for ubuntu 16.04,
+    // a vector is serializated
     std::vector<MapPoint*> mvpBackupMapPoints;
     std::vector<KeyFrame*> mvpBackupKeyFrames;
 
@@ -181,11 +214,10 @@ protected:
 
     long unsigned int mnInitKFid;
     long unsigned int mnMaxKFid;
-    //long unsigned int mnLastLoopKFid;
+    // long unsigned int mnLastLoopKFid;
 
     // Index related to a big change in the map (loop closure, global BA)
     int mnBigChangeIdx;
-
 
     // View of the map in aerial sight (for the AtlasViewer)
     GLubyte* mThumbnail;
@@ -200,9 +232,8 @@ protected:
 
     // Mutex
     std::mutex mMutexMap;
-
 };
 
-} //namespace ORB_SLAM3
+}  // namespace ORB_SLAM3
 
-#endif // MAP_H
+#endif  // MAP_H
